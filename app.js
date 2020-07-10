fd149011472cd7e33cbcb3de29f1a33a3473b300
Ivan <iyanez717@gmail.com>
2020-07-10 15:41:29 -0500

    Submit secrets
    Aviable submit new secrets

--- app.js
+++ app.js
@@ -37,7 +37,8 @@ mongoose.connect('mongodb://localhost:27017/userDB', {
 const userSchema = new mongoose.Schema({
   email: String,
   password: String,
-  googleId: String
+  googleId: String,
+  secret: String
 });
 userSchema.plugin(passportLocalMongoose);
 userSchema.plugin(findOrCreate);
@@ -65,7 +66,7 @@ passport.use(new GoogleStrategy({
     userProfileURL: 'https://www.googleapis.com/oauth2/v3/userinfo'
   },
   function(accessToken, refreshToken, profile, cb) {
-    console.log(profile);
+    //console.log(profile);
     User.findOrCreate({ googleId: profile.id }, function (err, user) {
       return cb(err, user);
     });
@@ -92,12 +93,44 @@ app.post("/register", function(req, res) {
   });
 });
 
-app.get("/secrets", function(req, res) {
+app.get("/submit", function(req, res){
   if (req.isAuthenticated()) {
-    res.render("secrets");
+    res.render("submit");
   } else {
     res.redirect("/login");
   }
+});
+
+app.post("/submit", function(req, res){
+  const submitedSecret = req.body.secret;
+
+  console.log(req.user.id);
+
+  User.findById(req.user.id, function(err, foundUser){
+    if (!err) {
+      if (foundUser) {
+        foundUser.secret = submitedSecret;
+        foundUser.save(function(){
+          res.redirect("/secrets");
+        });
+      }
+    }else{
+      console.log(err);
+    }
+  });
+
+});
+// All secrets on the DB
+app.get("/secrets", function(req, res) {
+  User.find({"secret": {$ne: null}}, function(err, foundUsers){
+    if (!err) {
+      if (foundUsers) {
+        res.render("secrets", {usersWithSecrets: foundUsers});
+      }
+    }else{
+      console.log(err);
+    }
+  });
 });
 
 app.get('/auth/google',
